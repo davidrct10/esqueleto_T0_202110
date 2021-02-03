2a3fe2a4c536359733f8bdf720d83cd019c380e2
David Ricardo Carrillo Torres <dr.carrillo@uniandes.edu.co>
2021-02-02 20:49:45 -0500

    Comentario sobre la actualizacion registrada en repositorio local

---
+++ src/module-info.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+/**
+ * @author david
+ *
+ */
+module esqueleto_T0_202110 {
+	exports test.data_structures;
+	exports controller;
+	exports view;
+	exports test.logic;
+	exports model.logic;
+	exports main;
+	exports model.data_structures;
+
+	requires junit;
+}
